014dd5c9d8629f060de5a941e52d1c1ed2851eb9
consuelita <adelita@operatorfoundation.org>
2024-02-18 11:30:27 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Straw", branch: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.4"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.9"),
+        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", branch: "release"),
-        .package(url: "https://github.com/OperatorFoundation/Net", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", from: "0.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
